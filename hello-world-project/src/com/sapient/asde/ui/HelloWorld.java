9d4ea29f8a915de49615a0b42732c9a4e4008c88
Yanglesh Jaiswal <yanjaisw@publicisgroupe.net>
2022-06-06 16:07:32 +0530

    first commit

---
+++ hello-world-project/src/com/sapient/asde/ui/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sapient.asde.ui;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi,"+ args[0]);
+
+    }
+}
